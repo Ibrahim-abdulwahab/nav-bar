94b9e12b191a3dc98205cba38c5a1af1da74159d
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-19 19:09:45 +0300

    finish the project for lower screen width

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-window.addEventListener('load', function(){
+const drop=document.getElementById("more");
+const content = document.getElementById("content2");
+let n=0;
 
+const dropDown =()=>{
+    if (n==0){
+        content.style.transform="translate(0px,70px)";
+        n=1;
+    }
+    else{
+        content.style.transform="translate(0px,-130px)";
+        n=0;
+    }
 
+}
 
-    
-})
+drop.addEventListener("click",dropDown)
