45f8031b9982c1bcf5c0ee6a9265778a18a25eb7
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-20 19:13:44 +0300

    improved js code

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 const drop=document.getElementById("more");
 const content = document.getElementById("content2");
-let n=0;
 
 const dropDown =()=>{
-    if (n==0){
-        content.style.transform="translate(0px,70px)";
-        n=1;
-    }
-    else{
-        content.style.transform="translate(0px,-130px)";
-        n=0;
-    }
-
+    content.classList.toggle("show");
 }
 
 drop.addEventListener("click",dropDown)
